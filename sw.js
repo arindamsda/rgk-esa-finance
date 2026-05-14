e06fb2b0ae51a7057fff01690172c073cb4f0ef3
arindamsda <arindam.sda@gmail.com>
2026-05-14 21:57:32 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+// RGK ESA Finance - Service Worker v2
+const CACHE = 'rgkar-esa-v2';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res && res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
